8b990c0a339f039a543efa92bd31f8c64af9c78e
learn typescript  with sr <syedarohab8@gmail.com>
2024-07-26 12:41:19 -0700

    Add files via upload

---
+++ operators.js
@@ -0,0 +1,10 @@
+"use strict";
+// boolean 
+//  greater  than  operators ;
+// let a = 100 ;
+// let b = 80 ;
+// console.log ( a >= b) ;
+// less than operators ;
+let x = 80;
+let y = 100;
+console.log(x <= y);
---
+++ operators.ts
@@ -0,0 +1,22 @@
+// boolean 
+
+//  greater  than  operators ;
+
+// greater than symbol = ( > )
+
+// let a = 100 ;
+
+// let b = 80 ;
+
+// console.log ( a >= b) ;
+
+
+// less than operators ;
+
+// less than symbol = ( < )
+
+// let x = 80 ;
+
+// let y = 100 ;
+
+// console.log ( x <= y ) ;
